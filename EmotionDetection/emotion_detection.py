e93c45e5cdb9acc812dd6a4e8e85d98811a56a0c
hagemarun2 <201ir050d@s.internet.ac.jp>
2025-04-29 07:30:42 -0400

    before server

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    # Define the URL for the sentiment analysis API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}    # Set the headers with the required model ID for the API
+
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        dominant_emotion = max(emotions, key=emotions.get)
+    # If the response status code is 500, set label and score to None
+        return {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness,
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        # Handle errors or unexpected responses
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
---
+++ test_emo.py
@@ -0,0 +1,3 @@
+from emotion_detection import emotion_detector
+result = emotion_detector("I love this new technology")
+print(result)
